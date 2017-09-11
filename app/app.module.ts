2bd86753b2a195ee59245965fdd6fb0d6c589bd3
LNKX <31756700+LNKX@users.noreply.github.com>
2017-09-11 21:30:34 +0800

    Add files via upload
    Split the main page into separate components, created a nomenclature for component creation.

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,11 +2,29 @@ import { BrowserModule} from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { MaterialModule} from '@angular/material';
-import { AppComponent } from './app.component';
+import { AppComponent} from './app.component';
+import { MainMenuToolbarComponent } from './main-menu-toolbar/main-menu-toolbar.component';
+import { MainSideNavigationComponent } from './main-side-navigation/main-side-navigation.component';
+import { KelvinEnergyComponent } from './kelvin-energy/kelvin-energy.component';
+import { KelvinEnergySiteDetailsComponent } from './kelvin-energy/kelvin-energy-site-details/kelvin-energy-site-details.component';
+import { KelvinEnergyFloorDetailsComponent } from './kelvin-energy/kelvin-energy-floor-details/kelvin-energy-floor-details.component';
+import { KelvinEnergyWallDetailsComponent } from './kelvin-energy/kelvin-energy-wall-details/kelvin-energy-wall-details.component';
+import { KelvinEnergyRoofAndCeilingDetailsComponent } from './kelvin-energy/kelvin-energy-roof-and-ceiling-details/kelvin-energy-roof-and-ceiling-details.component';
+import { KelvinEnergyGlazingDetailsComponent } from './kelvin-energy/kelvin-energy-glazing-details/kelvin-energy-glazing-details.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuToolbarComponent,
+    MainSideNavigationComponent,
+    KelvinEnergyComponent,
+    KelvinEnergySiteDetailsComponent,
+    KelvinEnergyFloorDetailsComponent,
+    KelvinEnergyWallDetailsComponent,
+    KelvinEnergyRoofAndCeilingDetailsComponent,
+    KelvinEnergyGlazingDetailsComponent,
+    
   ],
   imports: [
     BrowserModule,
